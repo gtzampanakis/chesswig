a92c34ac5ad69d013cf8edf7803eebaf0b8960a0
Giorgos Tzampanakis <g_tzampanakis@wargaming.net>
2024-05-17 12:40:06 +0300

    WIP

--- main.scm
+++ main.scm
@@ -5,7 +5,7 @@
 (define fen-empty "8/8/8/8/8/8/8/8 w KQkq - 0 1")
 (define fen-mate-in-2 "r1b2k1r/ppp1bppp/8/1B1Q4/5q2/2P5/PPP2PPP/R3R1K1 w - - 1 0")
 (define fen-mate-in-2-simplified
-  "5k2/4bpp1/8/1B1Q4/8/8/5PPP/4R1K1 w - - 1 1")
+  "5k1n/4bpp1/8/1B1Q4/8/8/5PPP/4R1KN w - - 1 1")
   
 (define fen-simple "6nk/8/8/8/8/8/8/KN6 w - - 0 1")
 
